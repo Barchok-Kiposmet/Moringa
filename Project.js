02703a6ebf203d2c91b1b6d074200eb2620454b3
Patrick Barchok Kiposmet <barchokpatrick@gmail.com>
2022-10-22 00:39:08 +0300

    Added the alert feature

--- Project.js
+++ Project.js
@@ -21,15 +21,37 @@ const submit = () => {
     var YY = date.slice(2,4)
     var MM = date.slice(5,7)
     var DD = date.slice(8,10)
+
+    const dates = [CC, YY, MM, DD]
     
     // formula for the the day of the week
-    var dayOfTheWeek = (((CC/4)-2*CC-1) + ((5*YY/4))+((26*(MM+1)/10))+DD)%7
 
-    console.log(dayOfTheWeek)
+    var dayOfTheWeek = Math.floor((((CC/4)-2*CC-1) + ((5*YY/4))+((26*(MM+1)/10))+DD)%7);
 
-   
     
+    // console.log(dayOfTheWeek)
     
+    // retrieve the days of the week and the corresponding names
+
+   const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+   const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    // Testing the result against the gender results
+
+   if (gender == "Male" & dayOfTheWeek == index) {
+            alert ('You were born on' + daysOfTheWeek[0] + 'Your Akan Name is' + maleNames[0])
+   } else {
+    alert('You were born on a' + daysOfTheWeek[0] + 'You Akan Name is' + femaleNames[0])
+   }
+
+//    if (gender == "Male" & dayOfTheWeek == 1) {
+//         alert ('You were born on a' + daysOfTheWeek[1] + 'Your Akan Name is' + maleNames[1])
+//     } else {
+//         alert('You were born on a' + daysOfTheWeek[1] + 'You Akan Name is' + femaleNames[1])
+//     }
+    
+    console.log(submit)
 
 
 }
