4bd4522d2a009e474370cba7d6d1a03087e14b91
Patrick Barchok Kiposmet <barchokpatrick@gmail.com>
2022-10-22 20:04:50 +0300

    Spaced the female results

--- Project.js
+++ Project.js
@@ -62,7 +62,7 @@ const submit = () => {
         if (gender == "Male") {
             document.getElementById("results").textContent = ('You were born on a' + ' ' + daysOfTheWeek[index] + '.' + ' ' + 'Your Akan Name is' + ' ' + maleNames[index])
         } else {
-            document.getElementById("results").textContent = ('You were born on a' + daysOfTheWeek[index] + 'You Akan Name is' + femaleNames[index])
+            document.getElementById("results").textContent = ('You were born on a' + ' ' + daysOfTheWeek[index] + '.' + 'You Akan Name is' + ' ' + femaleNames[index])
         }
 
     
